70383d17a8c5fb45022be7f543753d74c4936ce1
EngineeringDude123 <98351745+EngineeringDude123@users.noreply.github.com>
2022-04-19 21:01:30 -0500

    Add files via upload

---
+++ RoboBee4000.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+
+    class RoboBee4000 : IWorker
+    {
+
+        public string Job
+        {
+            get { return "Egg Care"; }
+        }
+        public void WorkTheNextShift()
+        {
+            // Do the work of three bees!
+        }
+    }
+}
